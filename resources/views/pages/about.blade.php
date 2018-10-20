861d926e9ad5d3838b2d956ba61cce22e9a1806b
Yun-Jan <yt266@njit.edu>
2018-10-20 14:15:15 -0400

    Edited about page text

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,7 +5,7 @@
 
 
 
-    <h1>This is my about page</h1>
+    <h1>This is about page</h1>
     <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.
 
 @endsection
