871c546ab1acf364d49028debdbbf30ceb80f91b
Yun-Jan <yt266@njit.edu>
2018-10-20 13:42:32 -0400

    Added thankyou file

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/contact','PagesController@contact')->name('contact');
 
 Route::post('/contact','PagesController@store')->name('contact.store');
 
-Route::get('/thanks/{name}/{last}','PagesController@thanks')->name('thanks');
+Route::get('/thanks/{name}','PagesController@thanks')->name('thanks');
