357d7accac1f8ce67683b8440a03a929717e3b61
Yun-Jan <yt266@njit.edu>
2018-10-22 18:27:38 -0400

    Edited head text

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/contact','PagesController@contact')->name('contact');
 Route::post('/contact','PagesController@store')->name('contact.store');
 
 Route::get('/thanks/{name}','PagesController@thanks')->name('thanks');
+
+
